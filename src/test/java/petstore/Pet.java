1a960bb0fce61cb7fbe6d10ef9b8950d685c2ee2
Douglas <douglas17_silva@hotmail.com>
2021-08-11 22:29:37 -0300

    Criado o teste para inclusão de pet

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,38 @@
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class Pet {
+
+    String uri = "https://petstore.swagger.io/v2/pet"; //endereço da entidade Pet
+
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    @Test //identifica o método ou função como um teste para o TestNg
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson("db/pet1.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+
+    }
+
+}
+
