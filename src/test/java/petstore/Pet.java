b35cd4acd56676e92077619045ca3907afaa95dd
Douglas <douglas17_silva@hotmail.com>
2021-08-12 22:04:15 -0300

    Completado o script excluirPet (get)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -58,7 +58,7 @@ public class Pet {
                 .body("category.name", is("dog")) //pode ser feito com "containsString"
                 .body("status", is("available"))
         .extract()
-                .path("tag.id")
+                .path("tag.id") //tentar com "containsString"
         ;
 
         System.out.println("O id da tag é: " + tagId);
@@ -80,6 +80,26 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Charlie Brown"))
                 .body("status", is("sold"))
+        ;
+
+    }
+
+    @Test(priority=4)
+    public void excluirPet() {
+        String petId = "2403198157";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+
         ;
 
     }
