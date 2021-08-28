6a4067842de819c6c5bfb4a88e90169fffed2e25
Douglas <douglas17_silva@hotmail.com>
2021-08-28 13:15:52 -0300

    Final da aula 2 - Leitura do arquivo users.csv como dataProvider do TestNGe utilizando para criar um Json Body parametrizado para inclusão de usuários

--- src/test/java/datadriven/UserDD.java
+++ src/test/java/datadriven/UserDD.java
@@ -1,14 +1,23 @@
 package datadriven;
 
+import netscape.javascript.JSObject;
+import org.json.JSONObject;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.Data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.put;
 import static org.hamcrest.CoreMatchers.is;
 
 public class UserDD {
@@ -16,15 +25,39 @@ public class UserDD {
     String uri = "https://petstore.swagger.io/v2/user\n";
     Data data;
 
+    @DataProvider //Provedor de dados para os testes
+    public Iterator<Object[]> provider() throws IOException {
+        List<Object[]> usersTestData  = new ArrayList<>();
+        String[] testCase = null; //é um array pq guarda todos os dados de um usuário
+        String linha = null;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("db/users.csv"));
+
+        while((linha = bufferedReader.readLine()) != null) {
+            testCase = linha.split(",");
+            usersTestData.add(testCase);
+        }
+
+        return usersTestData.iterator();
+    }
+
     @BeforeMethod
     public void setup() {
         data = new Data();
     }
 
-    @Test
-    public void incluirUsuario() throws IOException {
+    @Test(dataProvider = "provider")
+    public void incluirUsuario(String id, String username, String firstName, String lastName, String email, String password,String phone, String userStatus) throws IOException {
 //        String jsonBoby = Data.lerJson("db/user1.json"); //caso seja criado método estático
-        String jsonBody = data.lerJson("db/user1.json");
+        String jsonBody = new JSONObject()
+        .put("id", id)
+        .put("username", username)
+        .put("firstName", firstName)
+        .put("lastName", lastName)
+        .put("email", email)
+        .put("password", password)
+        .put("phone", phone)
+        .put("userStatus", userStatus)
+        .toString();
 
         String userId =
                 given()
--- src/test/java/utils/Data.java
+++ src/test/java/utils/Data.java
@@ -1,10 +1,14 @@
 package utils;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.List;
 
 public class Data {
 
@@ -17,10 +21,12 @@ public class Data {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    public Collection<String[]> lerCSV(String caminhoCSV) throws IOException {
-        Reader reader = Files.newBufferedReader(Paths.get(caminhoCSV));
+    public List<String[]> lerCSV(String caminhoCSV) throws IOException {
+        Reader reader = Files.newBufferedReader(Paths.get(caminhoCSV)); //Lê um texto
+        CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build(); //Abre como um csv - o n°1 siginifica a qtde de linhas que deverão ser ignoradas
+        List<String[]> users = csvReader.readAll(); //Lê todos os dados do csv
 
-        return null;
+        return users;
     }
 
 }
